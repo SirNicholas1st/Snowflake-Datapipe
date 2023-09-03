879669d9e3bf0e5bbdf61f1f19ba0fbf3f562a00
Tino B <bomantino@gmail.com>
2023-09-03 11:06:26 +0300

    fixed sql syntax and specified which database and schema to use

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,11 @@
 -- These sql statements are responsible for creating the needed tables.
 
+USE DATABASE "OPEN-METEO";
+USE SCHEMA "PUBLIC";
+
 -- create a customer id table
-CREATE TABLE IF NOT EXISTS CUSTOMER_IDS AS (
+
+CREATE TABLE IF NOT EXISTS CUSTOMER_IDS (
     customer_id_hash varchar,
     customer_id varchar
 )
